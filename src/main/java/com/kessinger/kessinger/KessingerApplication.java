22d454299fcf77419d5398e037f3b51fd114c5d2
Gustavo Furuhata <gustavo_furuhata@hotmail.com>
2017-09-25 03:22:55 -0300

    Correção de alguns países e adição de perfil

--- src/main/java/com/kessinger/kessinger/KessingerApplication.java
+++ src/main/java/com/kessinger/kessinger/KessingerApplication.java
@@ -2,6 +2,10 @@ package com.kessinger.kessinger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class KessingerApplication {
@@ -9,4 +13,5 @@ public class KessingerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KessingerApplication.class, args);
 	}
+    
 }
--- src/main/java/com/kessinger/kessinger/controller/UsuarioController.java
+++ src/main/java/com/kessinger/kessinger/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.kessinger.kessinger.controller;
 
 
 import com.kessinger.kessinger.model.Usuario;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import com.kessinger.kessinger.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,15 +12,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @PreAuthorize("hasAuthority('USUARIO')")
 @RequestMapping("/user")
 public class UsuarioController {
 
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public UsuarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
 
     @GetMapping
-    public String usuario(Model model) {
+    public String usuario() {
         return "usuario/index";
+    }
+
+
+    @GetMapping("/perfil")
+    public String listar(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        model.addAttribute("usuario", usuarioRepository.findByUsername(name).get());
+        return "/usuario/perfil";
     }
 
 }
